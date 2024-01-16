5452a55d6f4ced29e590271920aa8a2218ba1f01
yungughyeon <djaudwls7873@gmail.com>
2024-01-16 19:48:23 +0900

    storybook proptypes test

--- storybook-practice/src/components/ui/Button.jsx
+++ storybook-practice/src/components/ui/Button.jsx
@@ -1,7 +1,27 @@
 import S from "./style";
+import PropTypes from "prop-types";
 
 const Button = ({ ...args }) => {
   return <S.Button {...args}>{args.label}</S.Button>;
+};
+
+Button.propTypes = {
+  /**
+   * 버튼내의 글자를 입력해주세요
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * 버튼의 사이즈를 골라주세요.
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  /**
+   * 버튼의 형식을 골라주세요
+   */
+  variant: PropTypes.oneOf(["primary", "secondary"]),
+  /**
+   * 텍스트의 크기를 골라주세요.
+   */
+  fontSize: PropTypes.oneOf(["18px", "30px", "50px"]),
 };
 
 export default Button;
