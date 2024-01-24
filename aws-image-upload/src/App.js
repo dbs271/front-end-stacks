03a7dc27bd1d53ad98a451700c953062170357c9
yungughyeon <djaudwls7873@gmail.com>
2024-01-24 21:54:10 +0900

    dotenv

--- aws-image-upload/src/App.js
+++ aws-image-upload/src/App.js
@@ -10,8 +10,8 @@ function App() {
   useEffect(() => {
     // 1. aws 키 설정
     AWS.config.update({
-      accessKeyId: process.env.ACCESS_KEY,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+      accessKeyId: process.env.REACT_APP_ACCESS_KEY,
+      secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
     });
 
     // 2. aws s3 객체 생성
