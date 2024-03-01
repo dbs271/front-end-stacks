c187901e9805210c44622debb479c1aabe12bca3
yungughyeon <djaudwls7873@gmail.com>
2024-03-01 17:10:21 +0900

    git 잔디 cal-heatmap 만들기

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,26 +1,41 @@
-import { Cells, Celly, TimeLine } from "./styles/styled";
+import { Body, Cells, Celly, TimeLine, Weeks, Weeky } from "./styles/styled";
+
+const DayNames = {
+  1: "Mon",
+  3: "Wed",
+  5: "Fri",
+};
 
 function Cell() {
-  return <Celly className="timeline-cell"></Celly>;
+  return <Celly className="timeline-cells-cell"></Celly>;
 }
 
-function Months() {}
+function Week({ index }) {
+  return <Weeky className="timeline-weeks-week">{DayNames[index]}</Weeky>;
+}
 
 function Timeline({ days }) {
   let cells = Array.from(new Array(days));
+  let weeks = Array.from(new Array(7));
 
   return (
-    <div className="timeline">
+    <TimeLine className="timeline">
       <div className="timeline-months"></div>
-      <Celly className="timeline-cell-wrapper">
-        {cells.map((_, index) => (
-          <Cell key={index} />
-        ))}
-      </Celly>
-    </div>
+      <Body className="timeline-body">
+        <Weeks className="timeline-weeks">
+          {weeks.map((_, index) => (
+            <Week key={index} index={index} />
+          ))}
+        </Weeks>
+        <Cells className="timeline-cells">
+          {cells.map((_, index) => (
+            <Cell key={index} />
+          ))}
+        </Cells>
+      </Body>
+    </TimeLine>
   );
 }
-
 function App() {
   let days = 365;
   return <Timeline days={days} />;
--- todolist/src/styles/styled.js
+++ todolist/src/styles/styled.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
+export const TimeLine = styled.div`
+  /* .timeline의 스타일 */
+`;
+
 export const Cells = styled.div`
   display: inline-flex;
   flex-direction: column;
   flex-wrap: wrap;
-  height: calc((10px + 4px) * 8px);
+  height: ${(10 + 4) * 8}px;
 `;
 
 export const Celly = styled.div`
@@ -14,4 +18,23 @@ export const Celly = styled.div`
   margin: 2px;
   border-radius: 2px;
   background-color: rgba(0, 0, 0, 0.05);
+`;
+
+export const Body = styled.div`
+  display: flex;
+`;
+
+export const Weeks = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+`;
+
+export const Weeky = styled.div`
+  font-size: 10px;
+  height: 10px;
+  /* width: 10px; */
+  border: 1px solid transparent;
+  margin: 2px;
+  text-align: center;
+  vertical-align: middle;
 `;
